fb18050baf24d300470e27805ff08042c5a14b60
Armel Logbo <ledurr2016@yahoo.com>
2022-09-21 02:42:25 +0000

    inserer_produit

--- traitements/produits/ajout.php
+++ traitements/produits/ajout.php
@@ -1,8 +1,25 @@
-<?php echo "bonjour";
-
-echo $_POST['libelle'];
-echo $_POST['code'];
-echo $_POST['prix'];
-echo $_POST['description'];
 
 
+<?php
+include('../../database/Connect_dataBase.php');
+
+// var_dump($_POST);
+
+// die();
+if (isset($_POST['add_produit'])) {
+    $libelle =  $_POST['libelle'];
+    $code =  $_POST['code'];
+    $prix =  $_POST['prix'];
+    $desc  =  $_POST['description'];
+
+    $sql = "INSERT INTO `produit` (`libelle`, `code`, `prix`, `description`) VALUES ( '" . $libelle . "','" . $code . "','" . $prix . "','" . $desc . "');";
+    // echo $sql;
+    // die();
+    $result = $conn->query($sql);
+
+
+    header('Location:../../index.php');
+}
+
+
+?>
